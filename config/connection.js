52972140b90cc3fe5e408bab57bbcb72458c670d
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-06-18 16:16:36 -0700

    added to server.js to establish connection to database

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+//require dotenv to get password and username from env file
+require('dotenv').config();
+//require mysql
+const mysql = require('mysql2');
+
+//connect to database
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
+}
+
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,52 @@
+//require express, express handlebars, controllers, express-session, connection, connect session sequilize session.store and dotenv
+const express = require('express');
+const exphbs = require('express-handlebars');
+//const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require('dotenv').config();
+
+//start app and define port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//require User, Blog and Comment models
+const { User, Blog, Comment } = require('./models');
+
+
+//utilize app
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+//set up handlebars
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//set up session
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {
+        //session will expire in 30 minutes
+        expires: 30 * 60 * 1000
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+//initialize the sessions middleware for use with Express
+app.use(session(sess));
+
+//routes to be used by server
+app.use(routes);
+
+//sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
+});
+
