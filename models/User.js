b36b72d740845a0f44356205d851e48841530bca
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-06-18 15:18:44 -0700

    added to User.js model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+//require sequelize, connection, and bcrypt
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+const User =  sequelize.define(
+    'User',
+    {
+        //id
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement:true
+        },
+        //username
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        //password
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
+        }
+    },
+    {
+        //hooks
+        hooks: {
+            //set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                //encrypt password
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            }
+        },
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+    }
+);
+
+module.exports = User;
